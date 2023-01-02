c90fe20090f6da6af170203540c9dc2b543aa744
RanaAmirHussain <104395071+RanaAmirHussain@users.noreply.github.com>
2023-01-02 18:33:32 +0500

    new

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+// import Head from 'next/head'
+// import Image from 'next/image'
+import Text from 'react'
+import { Inter } from '@next/font/google'
+// import styles from '../styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <> 
+    <center>
+      <h1 style={{'color' : 'green'}} >"ASSALAMUALAIKUM"</h1>  <br /> <h3  style={{'color' : 'yellow'}} >THIS IS ME</h3> <br />  <br /> <br /> <h1 style={{'color' : 'red'}} >"RANA AMIR HUSSAIN "</h1> 
+      </center>
+
+          </>
+
+  )
+}
