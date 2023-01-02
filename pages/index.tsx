08dc1682a06a2283e60bbbc812b1a44ec3702e86
RanaRabees <104692519+RanaRabees@users.noreply.github.com>
2023-01-02 21:35:11 +0500

    ranarabees

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <> 
     <center>
-      <h1 style={{'color' : 'green'}} >"ASSALAMUALAIKUM"</h1>  <br /> <h3  style={{'color' : 'yellow'}} >THIS IS ME</h3> <br />  <br /> <br /> <h1 style={{'color' : 'red'}} >"RANA AMIR HUSSAIN "</h1> 
+      <h1 style={{'color' : 'green'}} >"ASSALAMUALAIKUM"</h1>  <br /> <h3  style={{'color' : 'yellow'}} >THIS IS ME</h3> <br />  <br /> <br /> <h1 style={{'color' : 'red'}} >"Rana Muhammad Rabees Hussain "</h1> 
       </center>
 
           </>
